6046e14fc99d30eb02e9bcbda4b6da06c00e4440
Ehbraheem <ehbraheem@gmail.com>
2022-08-28 04:08:47 +0100

    Remove excess permissions

--- course-04/project/c4-final-project-starter-code/client/src/components/Todos.tsx
+++ course-04/project/c4-final-project-starter-code/client/src/components/Todos.tsx
@@ -46,11 +46,6 @@ export class Todos extends React.PureComponent<TodosProps, TodosState> {
 
   onTodoCreate = async (event: React.ChangeEvent<HTMLButtonElement>) => {
     try {
-      if (!this.state.newTodoName) {
-        alert('Todo name must be specified')
-        return false
-      }
-
       const dueDate = this.calculateDueDate()
       const newTodo = await createTodo(this.props.auth.getIdToken(), {
         name: this.state.newTodoName,
